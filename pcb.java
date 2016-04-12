3ec89fd45c75e785a39afa3eec29321546484b82
NGprogrammer <Ngbrandon94@gmail.com>
2016-04-12 16:30:31 -0400

    update

--- pcb.java
+++ pcb.java
@@ -1,16 +1,31 @@
-
+/**
+ * This is the PCB class, progress control block.
+ */
 public class pcb {
+
 	public int jobnum;
+
 	public int priority; 
-	public int jobsize;
-	public int maxCpuTime;
-	public int currentTime;
 	
+	public int jobsize;
+	
+	public int maxCpuTime;
+	
+	public int currentTime;
+	/**
+	 * This function initialize the variables from the ??? i forgot 
+	 * @param arr
+	 */
 	public pcb(int [] arr){
+
 		jobnum = arr[1];
+
 		priority = arr[2];
+
 		jobsize = arr[3];
+
 		maxCpuTime = arr[4];
+
 		currentTime = arr[5];
 	}
 
